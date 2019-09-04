e20b44c33855829b4f4fab1671089b6cac65876c
jrexe <jrexejrexe@gmail.com>
2019-09-04 16:45:28 +0800

    修改最低版本限制
    增加post ssl 的请求

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -39,6 +39,7 @@ class Client
     }
 
     /**
+     * POST 请求不验证 SSL 针数
      * @param string $url 地址
      * @param string $postData 请求的数据
      * @param array $options 设置
@@ -71,10 +72,48 @@ class Client
         return $data;
     }
 
+    /**
+     * 使用双向证书POST请求
+     * @param string $url 接口地址
+     * @param string|array $param 参数
+     * @param string $cert_path 证书绝对路径
+     * @param string $key_path 密钥绝对路径
+     * @param int $second 超时时间
+     * @return bool|string
+     * @throws HttpCurlException
+     */
+    public static function curlPostWithCertAndKey($url, $param, $cert_path, $key_path, $second = 30)
+    {
+        $ch = curl_init();
+        //超时时间
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        //设置证书
+        //使用证书：cert 与 key 分别属于两个.pem文件
+        //默认格式为PEM，可以注释
+        curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
+        curl_setopt($ch, CURLOPT_SSLCERT, $cert_path);
+        //默认格式为PEM，可以注释
+        curl_setopt($ch, CURLOPT_SSLKEYTYPE, 'PEM');
+        curl_setopt($ch, CURLOPT_SSLKEY, $key_path);
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+        //返回结果
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new HttpCurlException($ch);
+        }
+        curl_close($ch);
+        return $data;
+    }
     // 下载图片
-
-
-
 
 
 }
